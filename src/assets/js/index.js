df5acc117f3cd7dbd7970e1abbf2e363ee005d96
Larry Botha <larry@fixate.it>
2018-03-19 11:09:30 +0100

    remove babel polyfill

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,12 @@
-import 'babel-polyfill';
 import formFactory from './lib/form';
 
 const formContainerElem = document.querySelector('.js-form-container');
 const successElem = document.querySelector('.js-success');
 const closeSuccessButton = document.querySelector('.js-reveal-form-button');
 
-formFactory({ formContainerElem, successElem });
+formFactory({formContainerElem, successElem});
 
-document.addEventListener('click', (e) => {
+document.addEventListener('click', e => {
   if (e.target === closeSuccessButton) {
     successElem.classList.add('is-hidden');
     formContainerElem.classList.remove('is-hidden');
